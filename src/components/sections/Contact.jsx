6e37f26a193e5354ae6c378442811a32d5fe15e2
santiagonotero <santiagonotero@hotmail.com>
2026-04-26 00:22:41 -0300

    Corregida la altura de las tarjetas de la sección Contacto

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -36,14 +36,14 @@ export default function Contact() {
 
         <div className="grid md:grid-cols-2 lg:grid-cols-2 gap-8">
           <RevealSection direction='up'>
-            <div className="group bg-white p-8 rounded-2xl shadow-lg hover:shadow-2xl transition-all duration-300 hover:-translate-y-2 border border-gray-100 justify-items-center">
+            <div className="group h-full bg-white p-8 rounded-2xl shadow-lg hover:shadow-2xl transition-all duration-300 hover:-translate-y-2 border border-gray-100 justify-items-center">
               <h3 className="text-2xl font-bold mb-4 text-gray-900">Nuestra dirección:</h3>
               <p className="text-xl text-gray-600"> Coronel Díaz 9798 - Palermo - CABA</p>
             </div>
           </RevealSection>
         
           <RevealSection direction='up'>
-            <div className="group bg-white p-8 rounded-2xl shadow-lg hover:shadow-2xl transition-all duration-300 hover:-translate-y-2 border border-gray-100 justify-items-center">
+            <div className="group h-full bg-white p-8 rounded-2xl shadow-lg hover:shadow-2xl transition-all duration-300 hover:-translate-y-2 border border-gray-100 justify-items-center">
               <h3 className="text-2xl font-bold mb-4 text-gray-900">Nuestros horarios</h3>
               <p className="text-xl text-gray-600">Lunes a viernes de 10 a 20hs</p>
               <p className="text-xl text-gray-600">Sábados de 10 a 17hs</p>
